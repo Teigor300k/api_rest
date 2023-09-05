02266f2a57d64df29f12baf1ea8a34899fca8477
Igor Texeira <114800507+black-logan@users.noreply.github.com>
2023-09-06 00:39:40 +0100

    Adicionei as rotas e a suas funções

--- app.js
+++ app.js
@@ -2,22 +2,80 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const auth  = require("./src/middlewares/auth");
+const Address = require("./src/models/Address");
+
+require("./src/database/Connection");
 app.use(express.json());
 
 app.get("/", async(req, res)=>{
+     try{
+        const address = await Address.findAll();
+
+        if(!address){
+            return res.status(400).send({message:"Dados não encontrado!"});
+        }
+
+        return res.status(200).send({data:address});
+
+     }catch(err){
+        return res.status(400).send({error : err})
+     }
    
 })
 
 app.post("/", async(req, res)=>{
-    res.send({msg: "Hello Word"})
+    
+    try{
+        const {country, city, street, number, district} = req.body;
+
+        const address = await Address.create({country, city, street, number, district});
+        if(!address){
+            return res.status(400).send({message:"Dados não encontrado!"});
+        }
+
+        return res.status(200).send({data:address});
+
+     }catch(err){
+        return res.status(400).send({error : err})
+     }
 })
 
-app.put("/", async(req, res)=>{
-    res.send({msg: "Hello Word"})
+app.put("/:id", async(req, res)=>{
+
+    try{
+        const {id} = req.params;
+
+        const {country, city, street, number, district} = req.body;
+
+        const address = await Address.update({country, city, street, number, district}, {where : {id} });
+
+        if(!address){
+            return res.status(400).send({message:"Dados não encontrado!"});
+        }
+
+        return res.status(200).send({data:address});
+
+     }catch(err){
+        return res.status(400).send({error : err})
+     }
 })
 
 app.delete("/", async(req, res)=>{
-    res.send({msg: "Hello Word"})
+    try{
+        const {id} = req.params;
+        
+        const address = await Address.destroy( {where : {id} });
+
+        if(!address){
+            return res.status(400).send({message:"Dados não encontrado!"});
+        }
+
+        return res.status(200).send({message:"Dados deletado com sucessso!"});
+
+     }catch(err){
+        return res.status(400).send({error : err});
+     }
 })
 
 
